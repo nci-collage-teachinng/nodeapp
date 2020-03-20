dae71378cd26f791bb550689e3ed470fd2ea0cca
Sumit <trsumit@users.noreply.github.com>
2020-03-20 19:43:45 +0000

    Update server.js

--- mysql/basicapp/server.js
+++ mysql/basicapp/server.js
@@ -21,26 +21,46 @@ const db = mysql.createConnection ({
 });
 
 
-
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
+router.post('/user', function(request, response){
+  console.log('in post method');
+
+let inset_query  = "insert into User_name (name) values ('name_placeholder_value')".replace('name_placeholder_value',request.body.name1 )
+
+db.query(inset_query , (error, result)=>{
+   if (error) {
+     console.log(error);
+     return response.sendStatus(500).send(error);
+   }
+}
+);
+});
+router.get('/user', function(request, response){
+  console.log('in user get method');
+
+  let select_query  = "select id, name from  User_name";
+
+  user_list ={}
+  db.query(select_query , (error, result)=>{
+    if (error) {
+
+       response.sendStatus(500).send(error);
+    }
+    result.forEach (record => {
+      user_list[record.id]  =   record.name
+    })
+     response.json(user_list);
+  });
+
+});
 
 
-
-
-
-
-
-//add the router
-app.use(express.static(__dirname + '/View'));
-//Store all HTML files in view folder.
 app.use(express.static(__dirname + '/Script'));
 //Store all JS and CSS in Scripts folder.
-
 app.use('/', router);
 app.listen(process.env.port || 3000);
-
 console.log('Running at Port 3000');
